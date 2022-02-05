45e1c70e11d2e27cb9f1da9d46cfa9761a581b2b
Josh Kersey <joshuakersey50@gmail.com>
2022-02-04 20:28:24 -0500

    Created hello_world rust application

---
+++ hello_rust/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    pritln!("Hello, world!");
+}
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/Users/joshkersey/Desktop/dev/rust/hello_rust/target/debug/hello_rust: /Users/joshkersey/Desktop/dev/rust/hello_rust/src/main.rs
